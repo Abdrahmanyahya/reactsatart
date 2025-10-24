9d0bcff29132d6d9da61c8e6e29cca1a40844854
Abdrahmanyahya <abdrahmanyahya21@gmail.com>
2025-10-24 16:32:08 +0300

    first componet navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Navbar from "./Component/Navbar/Navbar";'./Component/Navbar/Navbar.jsx'
+function App(){
+
+
+
+  return (
+<Fragment>
+  <Navbar></Navbar>
+</Fragment>
+  )
+}
+
+export default App;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import style from './Navbar.module.css';
+function Navbar(){
+    return (
+
+<div className={`${style.backgroundnav} d-flex justify-content-between p-4`}>
+    <a href="#" className={`${style.STARTfont} text-decoration-none text-white ms-4`}>START BOOTSTRAP</a>
+    <ul className={`${style.nav} me-5 mt-3`}>
+        <li><a href="#" className={'text-decoration-none text-white'}>PORTFOLIO</a></li>
+        <li><a href="#" className={'text-decoration-none text-white'}>ABOUT</a></li>
+        <li><a href="#" className={'text-decoration-none text-white'}>CONTACT</a></li>
+    </ul>
+</div>
+
+
+    )
+}
+export default Navbar;
